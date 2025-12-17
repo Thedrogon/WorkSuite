dfa6cc6aa9553f90f3cb23398ed8146f9bef0e49
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-12-17 23:59:33 +0530

    changes

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,14 +2,14 @@ import express from 'express'
 
 const app = express()
 
-const PORT = 5001
+const PORT = 5002
 
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
     
 })
 
-app.get('/api', (_,res) => {
+app.get('/hello', (_,res) => {
     res.json({'message': 'Hello World'})
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // server:{
+  //   port:5002,
+  //   proxy:{
+  //     '/api':{
+  //       target:'https://localhost:5002',
+  //       changeOrigin:false,
+  //       secure:false
+  //     }
+  //   }
+  // }
 })
